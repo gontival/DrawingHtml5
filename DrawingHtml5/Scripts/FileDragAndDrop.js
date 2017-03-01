d71df4dce5155bc2ca86c8cd1f573b32d9f8efc3
MCNEXT\jlevant <jlevant@mcnext.com>
2017-03-01 17:04:23 +0100

    Using FileList and File objects

---
+++ DrawingHtml5/Scripts/FileDragAndDrop.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="jquery-3.1.1.js" />
+$(document).ready(function () {
+    //jQuery.event.props.push()
+    $('#target').on('dragenter', preventDefault);
+    $('#target').on('dragover', preventDefault);
+    $('#target').on('drop', dropItem);
+});
+
+function preventDefault(e){
+    e.preventDefault();
+}
+
+function dropItem(e) {
+    e.dataTransfer = e.originalEvent.dataTransfer;
+    preventDefault(e);
+    var files = e.dataTransfer.files;
+    var $table = $('#fileInfo');
+    var i = 0;
+    $table.html(
+        '<thead><tr><th>Name</th><th>Type</th><th>Size</th></thead>');
+    for(i=0; i < files.length; i++)
+    {
+        $('<tr><td>'
+            + files[i].name + '</td><td>'
+            + files[i].type + '</td><td>'
+            + files[i].size + '</td><>tr>').appendTo($table);
+    }
+}
